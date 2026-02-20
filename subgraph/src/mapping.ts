ba913bcaca8aae330a05c2a6fb7fbade2e915e96
carloshenmes <153827510+carloshenmes@users.noreply.github.com>
2026-02-20 22:22:32 +0000

    feat: add subgraph for CarbonRegistry events indexing

--- subgraph/src/mapping.ts
+++ subgraph/src/mapping.ts
@@ -1,52 +1,78 @@
 import { BigInt, Bytes } from "@graphprotocol/graph-ts";
-import { CarbonRetired } from "../generated/CarbonRegistry/CarbonRegistry";
+import {
+  CarbonRetired,
+  DluzRewarded,
+  EnergyMinted,
+} from "../generated/CarbonRegistry/CarbonRegistry";
 import { Retirement, UserStats, ProtocolStats } from "../generated/schema";
 
-const DLUZ_RATE = BigInt.fromI32(10);
-const DENERGY_RATE = BigInt.fromI32(1);
 const PROTOCOL_ID = Bytes.fromHexString("0x01");
 
+function getOrCreateUserStats(user: Bytes): UserStats {
+  let stats = UserStats.load(user);
+  if (!stats) {
+    stats = new UserStats(user);
+    stats.totalRetired = BigInt.fromI32(0);
+    stats.totalDluzEarned = BigInt.fromI32(0);
+    stats.totalDenergyEarned = BigInt.fromI32(0);
+    stats.retirementCount = BigInt.fromI32(0);
+  }
+  return stats;
+}
+
+function getOrCreateProtocolStats(): ProtocolStats {
+  let stats = ProtocolStats.load(PROTOCOL_ID);
+  if (!stats) {
+    stats = new ProtocolStats(PROTOCOL_ID);
+    stats.totalCarbonRetired = BigInt.fromI32(0);
+    stats.totalDluzDistributed = BigInt.fromI32(0);
+    stats.totalDenergyMinted = BigInt.fromI32(0);
+    stats.totalRetirements = BigInt.fromI32(0);
+  }
+  return stats;
+}
+
 export function handleCarbonRetired(event: CarbonRetired): void {
-  // 1. Create Retirement entity
-  let retirement = new Retirement(event.transaction.hash);
-  retirement.user = event.params.user;
+  let id = event.transaction.hash.concatI32(event.logIndex.toI32());
+
+  let retirement = new Retirement(id);
+  retirement.user = event.params.retiree;
   retirement.amount = event.params.amount;
   retirement.reason = event.params.reason;
-  retirement.dluzReward = event.params.amount.times(DLUZ_RATE);
-  retirement.denergyMinted = event.params.amount.times(DENERGY_RATE);
+  retirement.dluzReward = BigInt.fromI32(0);
+  retirement.denergyMinted = BigInt.fromI32(0);
   retirement.blockNumber = event.block.number;
   retirement.blockTimestamp = event.block.timestamp;
   retirement.transactionHash = event.transaction.hash;
   retirement.save();
 
-  // 2. Update UserStats
-  let userId = event.params.user;
-  let userStats = UserStats.load(userId);
-  if (!userStats) {
-    userStats = new UserStats(userId);
-    userStats.totalRetired = BigInt.fromI32(0);
-    userStats.totalDluzEarned = BigInt.fromI32(0);
-    userStats.totalDenergyEarned = BigInt.fromI32(0);
-    userStats.retirementCount = BigInt.fromI32(0);
-  }
+  let userStats = getOrCreateUserStats(changetype<Bytes>(event.params.retiree));
   userStats.totalRetired = userStats.totalRetired.plus(event.params.amount);
-  userStats.totalDluzEarned = userStats.totalDluzEarned.plus(event.params.amount.times(DLUZ_RATE));
-  userStats.totalDenergyEarned = userStats.totalDenergyEarned.plus(event.params.amount.times(DENERGY_RATE));
   userStats.retirementCount = userStats.retirementCount.plus(BigInt.fromI32(1));
   userStats.save();
 
-  // 3. Update ProtocolStats (singleton)
-  let protocol = ProtocolStats.load(PROTOCOL_ID);
-  if (!protocol) {
-    protocol = new ProtocolStats(PROTOCOL_ID);
-    protocol.totalCarbonRetired = BigInt.fromI32(0);
-    protocol.totalDluzDistributed = BigInt.fromI32(0);
-    protocol.totalDenergyMinted = BigInt.fromI32(0);
-    protocol.totalRetirements = BigInt.fromI32(0);
-  }
+  let protocol = getOrCreateProtocolStats();
   protocol.totalCarbonRetired = protocol.totalCarbonRetired.plus(event.params.amount);
-  protocol.totalDluzDistributed = protocol.totalDluzDistributed.plus(event.params.amount.times(DLUZ_RATE));
-  protocol.totalDenergyMinted = protocol.totalDenergyMinted.plus(event.params.amount.times(DENERGY_RATE));
   protocol.totalRetirements = protocol.totalRetirements.plus(BigInt.fromI32(1));
+  protocol.save();
+}
+
+export function handleDluzRewarded(event: DluzRewarded): void {
+  let userStats = getOrCreateUserStats(changetype<Bytes>(event.params.retiree));
+  userStats.totalDluzEarned = userStats.totalDluzEarned.plus(event.params.amount);
+  userStats.save();
+
+  let protocol = getOrCreateProtocolStats();
+  protocol.totalDluzDistributed = protocol.totalDluzDistributed.plus(event.params.amount);
+  protocol.save();
+}
+
+export function handleEnergyMinted(event: EnergyMinted): void {
+  let userStats = getOrCreateUserStats(changetype<Bytes>(event.params.retiree));
+  userStats.totalDenergyEarned = userStats.totalDenergyEarned.plus(event.params.amount);
+  userStats.save();
+
+  let protocol = getOrCreateProtocolStats();
+  protocol.totalDenergyMinted = protocol.totalDenergyMinted.plus(event.params.amount);
   protocol.save();
 }
